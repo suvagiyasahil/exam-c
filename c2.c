47a128e57a0911c1d6950059218f28d8ee176413
tanish798 <kakadiyatanish6@gmail.com>
2025-01-29 18:31:45 +0530

    leb work1.2

---
+++ c2.c
@@ -0,0 +1,17 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <stdio.h>
+
+int main()
+{
+    int i=45;
+    printf("%d",i);
+
+    return 0;
+}
---
+++ q4.c
@@ -0,0 +1,16 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <stdio.h>
+
+int main()
+{
+    printf("* \n* \n* \n*        * *        *\n*       *   *      *\n*      *     *    *\n*     *       *  *\n*    *\n*   *\n*  *\n*");
+    
+    return 0;
+}
