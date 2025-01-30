7b123936bf25be86d883a66d789a315c2722bcd5
tanish798 <kakadiyatanish6@gmail.com>
2025-01-30 16:37:42 +0530

    work rearange

---
+++ leb 2.1/leb2.2 .c.c
@@ -0,0 +1,21 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <stdio.h>
+
+int main()
+{
+   
+    printf("----------");
+    printf("\n|         |");
+    printf("\n| 5*5=25  |");
+    printf("\n|         |");
+    printf("\n----------");
+
+    return 0;
+}
